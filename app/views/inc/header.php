2705b3bfa448fa2a8c3a610498edb465570f48f4
Dipankar Majumder <dipankarmajumder2000@outlook.com>
2020-01-22 21:58:08 +0530

    move img resources to localhost

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="shortcut icon" type="image/x-icon" href="/exam-management-php/public/images/favicon.ico">
+  <link rel="shortcut icon" type="image/x-icon" href="<?php echo URLROOT; ?>/images/favicon.ico">
   <!-- Import Bootstrap CSS -->
   <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous"> -->
   <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/bootstrap.css">
