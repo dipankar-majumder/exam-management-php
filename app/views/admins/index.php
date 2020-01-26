3a2e3714c70efff5ef24e6c3792297075e395dbc
Pritam Das <pd7662@gmail.com>
2020-01-26 21:28:10 +0530

    admin page added with feather icons

--- app/views/admins/index.php
+++ app/views/admins/index.php
@@ -1,1 +1,82 @@
-<h1>ADMIN INDEX</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" type="image/x-icon" href="<?php echo URLROOT; ?>/images/favicon.ico">
+    <!-- Import Bootstrap CSS -->
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous"> -->
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/bootstrap.css">
+    <!-- Import Font Awesome CSS -->
+    <!-- <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"> -->
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/fontawesome.css">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/dashboard.css">
+    <title><?php echo SITENAME; ?></title>
+</head>
+
+<body>
+<?php require APPROOT . '/views/inc/navbar.php'; ?>
+    <style>
+            .bd-placeholder-img {
+                font-size: 1.125rem;
+                text-anchor: middle;
+                -webkit-user-select: none;
+                -moz-user-select: none;
+                -ms-user-select: none;
+                user-select: none;
+            }
+
+            @media (min-width: 768px) {
+                .bd-placeholder-img-lg {
+                    font-size: 3.5rem;
+                }
+            }
+        </style>
+        <!-- Custom styles for this template -->
+
+    <body>
+
+
+    <div class="container-fluid">
+        <div class="row">
+            <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+                <div class="sidebar-sticky">
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">
+                                <span data-feather="activity"></span>
+                                Dashboard <span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <span data-feather="file"></span>
+                                Exams
+                            </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <span data-feather="users"></span>
+                                Teachers
+                            </a>
+                        </li>
+
+                </div>
+            </nav>
+
+            </main>
+        </div>
+    </div>
+
+    <?php require APPROOT . '/views/inc/footer.php'; ?>
+
+    <script src="https://getbootstrap.com/docs/4.4/examples/dashboard/dashboard.js"></script>
+
+    </body>
+    </html>
+
+
