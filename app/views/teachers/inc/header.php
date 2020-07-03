7e623bda351e950f35c461afc900f1b5afcbccda
ankush-bandyopadhyay <bandyopadhyayankush38@gmail.com>
2020-07-03 13:15:47 +0530

    test

--- app/views/teachers/inc/header.php
+++ app/views/teachers/inc/header.php
@@ -5,8 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Teacher's Portal</title>
-  <!-- MaterializeCSS -->
-  <!-- <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/materialize.css"> -->
+  <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/bootstrap.css">
   <link rel="stylesheet" href="<?php echo URLROOT; ?>/icons/material-design-icons/material-icons.css">
 </head>
 
--- app/views/teachers/inc/navbar.php
+++ app/views/teachers/inc/navbar.php
@@ -1,4 +1,4 @@
-<nav>
+<!-- <nav>
   <ul>
     <li><a href="<?php echo URLROOT; ?>/teachers">Home</a></li>
     <li>Exam Duties
@@ -10,4 +10,19 @@
     </li>
     <li><a href="<?php echo URLROOT; ?>/teachers/logout">Logout</a></li>
   </ul>
+</nav> -->
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Home</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Exam duties</a></li>
+      <li><a href="#">Qustion paper</a></li>
+      <li><a href="#">External</a></li>
+      <li><a href="#">Answer paper</a></li>
+      <li><a href="#">Logout</a></li>
+    </ul>
+  </div>
 </nav>
